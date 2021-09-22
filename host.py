444210ddd16487e51c970705b9abfd9e6080ae3c
bobmarley-tech <56273590+bobmarley-tech@users.noreply.github.com>
2021-09-22 20:19:10 +0100

    added the basic api
    features (so far):
    1. ip locking
    2. send configs to user

---
+++ host.py
@@ -0,0 +1,33 @@
+from flask.app import Flask, request, jsonify
+from flask.helpers import send_file
+app = Flask(__name__)
+
+@app.route("/api/configs", methods = ["GET", "POST"])
+async def index():
+    headers = request.headers
+    key = headers.get("Authorization")
+    ipnow = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    ipcombo = f"{key}:{ipnow}"
+    noip = f"{key}:"
+    print(ipnow)
+    with open("./api/data.txt", "r") as databasefile:
+        lolxdddd = databasefile.read()
+        if ipcombo in lolxdddd:
+            jsonify({"message": f"{key} iS Locked To {ipnow}"}), 200
+            return send_file("configs.zip"), 200
+        elif noip in lolxdddd:
+            print("first time")
+            writeout = lolxdddd.replace(f"{key}:", f"{key}:{ipnow}\n")
+            with open('./api/data.txt', 'w') as file:
+                file.write(writeout)
+            return jsonify({"message": f"Were Locking {key} to your ip: {ipnow}"})
+        else: 
+            print("wrong")
+            return jsonify({"message": "ERROR: Incorrect Key"}), 401
+
+@app.route("/api/users",methods = ["GET", "POST"])
+async def users():
+    Headers = request.headers
+
+
+app.run(host='PUT IPV4 HERE (:', port = 8000, threaded = True, debug = True)
