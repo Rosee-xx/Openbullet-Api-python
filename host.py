01a73166e8acb3aea0fa2e73adbf614ee86da8c1
bobmarley-tech <56273590+bobmarley-tech@users.noreply.github.com>
2021-09-25 12:06:15 +0100

    added the funtion to add keys
    i did a small update to add the keys sent to it to the database and now it works coolio (p.s. i wanna die)
    (p.s. im streaming on twitch while coding this @ twitch.tv/roseezzzz)
    come look at next stream

--- host.py
+++ host.py
@@ -1,5 +1,7 @@
+from flask import Flask
 from flask.app import Flask, request, jsonify
 from flask.helpers import send_file
+from werkzeug.datastructures import Headers
 app = Flask(__name__)
 
 @app.route("/api/configs", methods = ["GET", "POST"])
@@ -8,7 +10,7 @@ async def index():
     key = headers.get("Authorization")
     ipnow = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     ipcombo = f"{key}:{ipnow}"
-    noip = f"{key}:"
+    noip = f"{key}"
     print(ipnow)
     with open("./api/data.txt", "r") as databasefile:
         lolxdddd = databasefile.read()
@@ -17,7 +19,7 @@ async def index():
             return send_file("configs.zip"), 200
         elif noip in lolxdddd:
             print("first time")
-            writeout = lolxdddd.replace(f"{key}:", f"{key}:{ipnow}\n")
+            writeout = lolxdddd.replace(f"{key}", f"{key}:{ipnow}\n")
             with open('./api/data.txt', 'w') as file:
                 file.write(writeout)
             return jsonify({"message": f"Were Locking {key} to your ip: {ipnow}"})
@@ -25,9 +27,22 @@ async def index():
             print("wrong")
             return jsonify({"message": "ERROR: Incorrect Key"}), 401
 
-@app.route("/api/users",methods = ["GET", "POST"])
+@app.route("/api/users", methods = ["GET", "POST"])
 async def users():
-    Headers = request.headers ## coming soon (; adding a feature to add users very nice ikr
+    Headers = request.headers
+    key = Headers.get("secret_key")
+    addthiskeyz = Headers.get("addthiskey")
+    if key == "uiyawndiuawmndd":
+        with open("./api/data.txt", "w") as file:
+            file.write(addthiskeyz)
+            return jsonify({"message": f"Your Key {addthiskeyz} Has Been Added To The Keys List \n First Time Connect Will Lock The Code To Your IP Share It And Youll Get Banned (:"}), 200
+    else: 
+        return jsonify({"message": "ERROR: You Have The Wrong Secret Key."}), 401
 
+@app.route("/api/users/delete", methods = ["GET", "POST"])
+async def delete_user():
+    Headers = request.headers
+    # coming soon cuties
+        
 
-app.run(host='PUT IPV4 HERE (:', port = 8000, threaded = True, debug = True)
+app.run(host='put ip v4 here (:', port = 8000, threaded = True, debug = True)
